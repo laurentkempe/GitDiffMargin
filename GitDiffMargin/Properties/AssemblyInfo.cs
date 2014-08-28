a98e3dd1c76710feb11358d7c588cd05987d6407
Laurent Kempé <laurent.kempe@innoveo.com>
2014-08-28 23:19:45 +0200

    Update version to 2.0.0

--- GitDiffMargin/Properties/AssemblyInfo.cs
+++ GitDiffMargin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitDiffMargin")]
-[assembly: AssemblyDescription("A margin extension to the Visual Studio Editor which displays the git diff.")]
+[assembly: AssemblyTitle("Git Diff Margin")]
+[assembly: AssemblyDescription("Git Diff Margin displays live changes of the currently edited file on Visual Studio 2013 margin and scroll bar.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Laurent Kempé")]
-[assembly: AssemblyProduct("GitDiffMargin")]
+[assembly: AssemblyProduct("Git Diff Margin")]
 [assembly: AssemblyCopyright("Laurent Kempé")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.9.2")]
-[assembly: AssemblyFileVersion("1.3.9.2")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
