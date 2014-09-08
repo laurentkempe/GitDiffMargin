492462bf8bce7560554be82ce46f4e3d3d264e97
Sam Harwell <sam.harwell@rackspace.com>
2014-09-08 12:29:55 -0500

    Use IVsToolbarTrayHost to provide the toolbar functionality

--- GitDiffMargin/ViewModel/EditorDiffViewModel.cs
+++ GitDiffMargin/ViewModel/EditorDiffViewModel.cs
@@ -1,13 +1,21 @@
 ﻿#region using
 
 using System;
+using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using GalaSoft.MvvmLight.Command;
 using GitDiffMargin.Core;
 using GitDiffMargin.Git;
-using System.Linq;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using IOleCommandTarget = Microsoft.VisualStudio.OLE.Interop.IOleCommandTarget;
+using OLECMD = Microsoft.VisualStudio.OLE.Interop.OLECMD;
+using OLECMDF = Microsoft.VisualStudio.OLE.Interop.OLECMDF;
+using OleConstants = Microsoft.VisualStudio.OLE.Interop.Constants;
 
 #endregion
 
@@ -21,6 +29,9 @@ namespace GitDiffMargin.ViewModel
         private ICommand _copyOldTextCommand;
         private ICommand _rollbackCommand;
         private ICommand _showPopUpCommand;
+
+        private EditorDiffMarginViewModel _diffMarginViewModel;
+        private IVsToolbarTrayHost _toolbarTrayHost;
 
         internal EditorDiffViewModel(HunkRangeInfo hunkRangeInfo, IMarginCore marginCore, Action<DiffViewModel, HunkRangeInfo> updateDiffDimensions)
             : base(hunkRangeInfo, marginCore, updateDiffDimensions)
@@ -124,7 +135,29 @@ namespace GitDiffMargin.ViewModel
 
         public ICommand ShowPopUpCommand
         {
-            get { return _showPopUpCommand ?? (_showPopUpCommand = new RelayCommand(ShowPopUp)); }
+            get { return _showPopUpCommand ?? (_showPopUpCommand = new RelayCommand<EditorDiffMarginViewModel>(ShowPopUp)); }
+        }
+
+        public object ToolBarTray
+        {
+            get
+            {
+                if (_toolbarTrayHost == null)
+                    return null;
+
+                IVsUIElement toolbarTray;
+                ErrorHandler.ThrowOnFailure(_toolbarTrayHost.GetToolbarTray(out toolbarTray));
+
+                object uiObject;
+                ErrorHandler.ThrowOnFailure(toolbarTray.GetUIObject(out uiObject));
+
+                IVsUIWpfElement wpfElement = uiObject as IVsUIWpfElement;
+
+                object frameworkElement;
+                ErrorHandler.ThrowOnFailure(wpfElement.GetFrameworkElement(out frameworkElement));
+
+                return frameworkElement;
+            }
         }
 
         public bool ShowPopup
@@ -134,7 +167,29 @@ namespace GitDiffMargin.ViewModel
             {
                 if (value == _showPopup) return;
                 _showPopup = value;
+                if (value)
+                {
+                    IVsUIShell4 uiShell = Package.GetGlobalService(typeof(SVsUIShell)) as IVsUIShell4;
+                    if (uiShell != null)
+                    {
+                        IVsToolbarTrayHost toolbarTrayHost;
+                        ErrorHandler.ThrowOnFailure(uiShell.CreateToolbarTray(new ToolbarCommandHandler(this), out toolbarTrayHost));
+
+                        Guid toolBarGuid = typeof(GitDiffMarginCommand).GUID;
+                        ErrorHandler.ThrowOnFailure(toolbarTrayHost.AddToolbar(ref toolBarGuid, (int)GitDiffMarginCommand.GitDiffToolbar));
+
+                        _toolbarTrayHost = toolbarTrayHost;
+                    }
+                }
+                else
+                {
+                    var toolbarTrayHost = Interlocked.Exchange(ref _toolbarTrayHost, null);
+                    if (toolbarTrayHost != null)
+                        ErrorHandler.CallWithCOMConvention(() => toolbarTrayHost.Close());
+                }
+
                 RaisePropertyChanged(() => ShowPopup);
+                RaisePropertyChanged(() => ToolBarTray);
             }
         }
 
@@ -214,9 +269,116 @@ namespace GitDiffMargin.ViewModel
             MarginCore.TextView.VisualElement.Focus();
         }
 
-        private void ShowPopUp()
+        private void ShowPopUp(EditorDiffMarginViewModel diffMarginViewModel)
         {
+            _diffMarginViewModel = diffMarginViewModel;
             ShowPopup = true;
+        }
+
+        private class ToolbarCommandHandler : IOleCommandTarget
+        {
+            private readonly EditorDiffViewModel _viewModel;
+
+            public ToolbarCommandHandler(EditorDiffViewModel viewModel)
+            {
+                _viewModel = viewModel;
+            }
+
+            public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+            {
+                if (pguidCmdGroup == typeof(GitDiffMarginCommand).GUID)
+                {
+                    switch ((GitDiffMarginCommand)prgCmds[0].cmdID)
+                    {
+                    case GitDiffMarginCommand.PreviousChange:
+                        if (_viewModel._diffMarginViewModel == null)
+                        {
+                            prgCmds[0].cmdf = (uint)OLECMDF.OLECMDF_SUPPORTED;
+                            return VSConstants.S_OK;
+                        }
+
+                        prgCmds[0].cmdf = ToCommandFlags(_viewModel._diffMarginViewModel.PreviousChangeCommand.CanExecute(_viewModel));
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.NextChange:
+                        if (_viewModel._diffMarginViewModel == null)
+                        {
+                            prgCmds[0].cmdf = (uint)OLECMDF.OLECMDF_SUPPORTED;
+                            return VSConstants.S_OK;
+                        }
+
+                        prgCmds[0].cmdf = ToCommandFlags(_viewModel._diffMarginViewModel.NextChangeCommand.CanExecute(_viewModel));
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.RollbackChange:
+                        prgCmds[0].cmdf = ToCommandFlags(_viewModel.RollbackCanExecute());
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.ShowDiff:
+                        prgCmds[0].cmdf = ToCommandFlags(_viewModel.ShowDifferenceCanExecute());
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.CopyOldText:
+                        prgCmds[0].cmdf = ToCommandFlags(_viewModel.CopyOldTextCanExecute());
+                        return VSConstants.S_OK;
+
+                    default:
+                        prgCmds[0].cmdf = 0;
+                        return VSConstants.S_OK;
+                    }
+                }
+
+                prgCmds[0].cmdf = 0;
+                return VSConstants.S_OK;
+            }
+
+            public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+            {
+                if (pguidCmdGroup == typeof(GitDiffMarginCommand).GUID)
+                {
+                    switch ((GitDiffMarginCommand)nCmdID)
+                    {
+                    case GitDiffMarginCommand.PreviousChange:
+                        if (_viewModel._diffMarginViewModel == null)
+                            return (int)OleConstants.MSOCMDERR_E_NOTSUPPORTED;
+
+                        _viewModel._diffMarginViewModel.PreviousChangeCommand.Execute(_viewModel);
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.NextChange:
+                        if (_viewModel._diffMarginViewModel == null)
+                            return (int)OleConstants.MSOCMDERR_E_NOTSUPPORTED;
+
+                        _viewModel._diffMarginViewModel.NextChangeCommand.Execute(_viewModel);
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.RollbackChange:
+                        _viewModel.Rollback();
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.ShowDiff:
+                        _viewModel.ShowDifference();
+                        return VSConstants.S_OK;
+
+                    case GitDiffMarginCommand.CopyOldText:
+                        _viewModel.CopyOldText();
+                        return VSConstants.S_OK;
+
+                    default:
+                        return (int)OleConstants.MSOCMDERR_E_NOTSUPPORTED;
+                    }
+                }
+
+                return (int)OleConstants.MSOCMDERR_E_UNKNOWNGROUP;
+            }
+
+            private static uint ToCommandFlags(bool canExecute)
+            {
+                if (canExecute)
+                    return (uint)(OLECMDF.OLECMDF_SUPPORTED | OLECMDF.OLECMDF_ENABLED);
+                else
+                    return (uint)OLECMDF.OLECMDF_SUPPORTED;
+            }
         }
     }
 }
