b301e7cc048caa747097cd353301dfadfaa1e5bf
Sam Harwell <sam.harwell@rackspace.com>
2014-09-08 12:06:42 -0500

    Define toolbar commands for the Git Diff Margin actions

---
+++ GitDiffMargin/GitDiffMarginCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace GitDiffMargin
+{
+    using System.Runtime.InteropServices;
+
+    [Guid("691DB887-6D82-46A9-B0AF-407C7F0E39BE")]
+    public enum GitDiffMarginCommand
+    {
+        PreviousChange = 0,
+        NextChange = 1,
+        RollbackChange = 2,
+        ShowDiff = 3,
+        CopyOldText = 4,
+
+        GitDiffToolbar = 100,
+
+        GitDiffToolbarGroup = 150,
+    }
+}
--- GitDiffMargin/GitDiffMarginPackage.cs
+++ GitDiffMargin/GitDiffMarginPackage.cs
@@ -1,12 +1,49 @@
 ﻿namespace GitDiffMargin
 {
+    using System;
+    using System.ComponentModel.Design;
     using System.Runtime.InteropServices;
     using Microsoft.VisualStudio.Shell;
 
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [Guid("F82C1EF6-3B52-425E-BC28-4934E6073A32")]
 
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+
     public class GitDiffMarginPackage : Package
     {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var menuCommandService = (MenuCommandService)GetService(typeof(IMenuCommandService));
+            AddCommand(menuCommandService, GitDiffMarginCommand.PreviousChange);
+            AddCommand(menuCommandService, GitDiffMarginCommand.NextChange);
+            AddCommand(menuCommandService, GitDiffMarginCommand.RollbackChange);
+            AddCommand(menuCommandService, GitDiffMarginCommand.ShowDiff);
+            AddCommand(menuCommandService, GitDiffMarginCommand.CopyOldText);
+        }
+
+        private void AddCommand(MenuCommandService menuCommandService, GitDiffMarginCommand command)
+        {
+            EventHandler invokeHandler = HandleCommandInvoke;
+            EventHandler changeHandler = HandleCommandChange;
+            EventHandler beforeQueryStatus = HandleCommandBeforeQueryStatus;
+            CommandID id = new CommandID(typeof(GitDiffMarginCommand).GUID, (int)command);
+            string text = null;
+            menuCommandService.AddCommand(new OleMenuCommand(invokeHandler, changeHandler, beforeQueryStatus, id, text));
+        }
+
+        private void HandleCommandInvoke(object sender, EventArgs e)
+        {
+        }
+
+        private void HandleCommandChange(object sender, EventArgs e)
+        {
+        }
+
+        private void HandleCommandBeforeQueryStatus(object sender, EventArgs e)
+        {
+        }
     }
 }
