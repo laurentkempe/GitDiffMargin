8fd86ae6c11f6db9d27f82ea2f3d908e359c0560
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2015-08-08 01:14:55 +0200

    Update GitDiffMargin Chocolatey package

--- ChocolateyPackage/GitDiffMargin/tools/chocolateyInstall.ps1
+++ ChocolateyPackage/GitDiffMargin/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ try {
 
   $params = @{
     PackageName = $package;
-    VsixUrl = 'http://visualstudiogallery.msdn.microsoft.com/cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/10/GitDiffMargin.vsix';
+    VsixUrl = 'https://visualstudiogallery.msdn.microsoft.com/cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/12/GitDiffMargin.vsix';
   }
 
   Install-ChocolateyVsixPackage @params
