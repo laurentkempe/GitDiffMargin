dfaa74e3eac997f22672418d8473e1cf0633da47
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2018-08-25 15:59:32 +0000

    Delete unused local variable

--- GitDiffMargin/Core/DiffUpdateBackgroundParser.cs
+++ GitDiffMargin/Core/DiffUpdateBackgroundParser.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -85,8 +84,6 @@ namespace GitDiffMargin.Core
         {
             try
             {
-                var stopwatch = Stopwatch.StartNew();
-
                 var snapshot = TextBuffer.CurrentSnapshot;
                 if (!TextDocumentFactoryService.TryGetTextDocument(_documentBuffer, out var textDocument)) return;
 
