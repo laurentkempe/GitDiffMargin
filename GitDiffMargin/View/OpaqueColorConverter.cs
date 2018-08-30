94ae98f9e5d8936f74adb87e97232df62e91a9ef
Charles Milette <me@charlesmilette.net>
2018-08-30 19:49:15 -0400

    Fix MoeIDE conflict
    Fixes #137

---
+++ GitDiffMargin/View/OpaqueColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GitDiffMargin.View
+{
+    [ValueConversion(typeof(Brush), typeof(Brush))]
+    class OpaqueColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Brush))
+            {
+                throw new ArgumentException("Target type is not a brush.");
+            }
+
+            var newBrush = ((Brush)value).Clone();
+            newBrush.Opacity = 1.0; // Remove transparency
+
+            return newBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // We don't need to convert back. What should we do anyways? We don't know the original opacity, nor we can we get it.
+            throw new NotSupportedException();
+        }
+    }
+}
