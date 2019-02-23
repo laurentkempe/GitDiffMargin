96a37391b496178ed6f165ee1122743b0eba179f
Gary Ewan Park <gep13@gep13.co.uk>
2019-02-23 11:01:05 +0000

    Remove try/catch block
    - This is no longer required, as Chocolatey wraps this up internally

--- ChocolateyPackage/GitDiffMargin/tools/chocolateyInstall.ps1
+++ ChocolateyPackage/GitDiffMargin/tools/chocolateyInstall.ps1
@@ -1,15 +1,8 @@
-try {
-  $package = 'GitDiffMargin'
+$package = 'GitDiffMargin'
 
-  $params = @{
-    PackageName = $package;
-    VsixUrl = 'https://visualstudiogallery.msdn.microsoft.com/cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/12/GitDiffMargin.vsix';
-  }
-
-  Install-ChocolateyVsixPackage @params
-
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+$params = @{
+  PackageName = $package;
+  VsixUrl = 'https://visualstudiogallery.msdn.microsoft.com/cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/12/GitDiffMargin.vsix';
 }
+
+Install-ChocolateyVsixPackage @params
