abdfe6efe0bf0b7e81ca8acfe93a2ba7cfa8e6b2
Gary Ewan Park <gep13@gep13.co.uk>
2019-02-23 11:05:57 +0000

    Switch to using extension package
    - And Install-VisualStudioVsixExtension rather than Install-ChocolateyVsixPackage
    - This will mean that package installation will work for VS 2017, as well as older versions

--- ChocolateyPackage/GitDiffMargin/tools/chocolateyInstall.ps1
+++ ChocolateyPackage/GitDiffMargin/tools/chocolateyInstall.ps1
@@ -5,4 +5,4 @@ $params = @{
   VsixUrl = 'https://visualstudiogallery.msdn.microsoft.com/cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/12/GitDiffMargin.vsix';
 }
 
-Install-ChocolateyVsixPackage @params
+Install-VisualStudioVsixExtension @params
