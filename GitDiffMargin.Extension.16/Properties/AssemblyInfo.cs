548b527392a6175fc6c3ea9a03a172828e14692b
Sam Harwell <Sam.Harwell@microsoft.com>
2021-05-11 09:40:03 -0700

    Create dev17 and wrapper extensions

--- GitDiffMargin.Extension.16/Properties/AssemblyInfo.cs
+++ GitDiffMargin.Extension.16/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GitDiffMargin.Extension")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Laurent Kempé")]
-[assembly: AssemblyProduct("GitDiffMargin.Extension")]
-[assembly: AssemblyCopyright("Laurent Kempé")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -23,17 +10,3 @@ using Microsoft.VisualStudio.Shell;
 [assembly: Guid("e4701f35-8030-418e-8e8c-6ae72e229138")]
 
 [assembly: ProvideCodeBase(CodeBase = "$PackageFolder$\\GitDiffMargin.16.dll")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.0.0")]
-[assembly: AssemblyFileVersion("3.9.0.0")]
-[assembly: AssemblyInformationalVersion("3.9.0.0")]
