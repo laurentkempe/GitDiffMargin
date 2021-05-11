4d382b2cd3d25d813aad643675e666aebeada8e7
Sam Harwell <Sam.Harwell@microsoft.com>
2021-05-11 14:31:44 -0700

    Use the same name for implementation assemblies
    This change ensures Icons.imagemanifest is valid for all versions.

--- GitDiffMargin.Extension.16/Properties/AssemblyInfo.cs
+++ GitDiffMargin.Extension.16/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e4701f35-8030-418e-8e8c-6ae72e229138")]
 
-[assembly: ProvideCodeBase(CodeBase = "$PackageFolder$\\GitDiffMargin.16.dll")]
+[assembly: ProvideCodeBase(CodeBase = "$PackageFolder$\\GitDiffMargin.Impl.dll")]
